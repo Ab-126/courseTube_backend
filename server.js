d4dd410419a2aab5f632e988d0f7453b107ed58a
unknown <geekisaprogrammer@gmial.com>
2023-03-12 11:12:22 +0530

    second commit

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import app from "./app.js";
-import { connectDB } from "./config/Database.js";
+import { connectDB } from "./config/database.js";
 import cloudinary from "cloudinary";
 import RazorPay from "razorpay";
 import nodeCron from "node-cron";
